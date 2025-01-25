7f4099ae71123e9b5cc4bdaed5930ad63b06f84e
Ahmet Issa <ahmetissa@Issas-MacBook-Pro.local>
2025-01-26 01:29:33 +0500

    siu

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>YEAAAAAAH</h1>
+      <h1>SIUU</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 3)}>
           count is {count}
